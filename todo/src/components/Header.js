e54a3797b33f3b73f1fe56d9a967f5c9a1174fcf
Jason Ceballos <jceballos@icstars.org>
2021-05-19 02:13:19 -0500

    Final To-Do List

--- todo/src/components/Header.js
+++ todo/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='header'>
-            <h1>To Do List</h1>
+            <h1>To-Do List</h1>
         </div>
     )
 }
